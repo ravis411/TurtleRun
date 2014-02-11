87f01d03bcab84ab3f1ca7b7469bd6b736635e0e
liueric <liueric@usc.edu>
2014-02-10 20:38:24 -0800

    Initial Project Template

---
+++ Turtle Run/Turtle Run/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Turtle Run
+//
+//  Created by Eric Liu on 2/10/14.
+//  Copyright USC 2014. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
