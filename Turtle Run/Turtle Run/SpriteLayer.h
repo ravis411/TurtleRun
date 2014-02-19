fd87657a65a3b08239e596d2c1ee46f805f92c9f
Kyle Swinney <kmswinney@gmail.com>
2014-02-18 22:42:32 -0800

    Added SpriteLayer
    Still needs to be implemented

---
+++ Turtle Run/Turtle Run/SpriteLayer.h
@@ -0,0 +1,16 @@
+//
+//  SpriteLayer.h
+//  Turtle Run
+//
+//  Created by Kyle on 2/18/14.
+//  Copyright (c) 2014 USC. All rights reserved.
+//
+
+#import "CCLayer.h"
+#import "GroundLayer.h"
+
+@interface SpriteLayer : CCLayer {
+    GroundLayer *ground;
+}
+
+@end
---
+++ Turtle Run/Turtle Run/SpriteLayer.m
@@ -0,0 +1,17 @@
+//
+//  SpriteLayer.m
+//  Turtle Run
+//
+//  Created by Kyle on 2/18/14.
+//  Copyright (c) 2014 USC. All rights reserved.
+//
+
+#import "SpriteLayer.h"
+
+@implementation SpriteLayer {
+    super (init) {
+        
+    }
+}
+
+@end
