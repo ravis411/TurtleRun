f2fe4b20008f55705666dca9a11cf775e066bffb
Kyle Swinney <kmswinney@gmail.com>
2014-02-19 17:33:30 -0800

    Added Obstacle Class
    Yet to be implemented

---
+++ Turtle Run/Turtle Run/Obstacle.h
@@ -0,0 +1,15 @@
+//
+//  Obstacle.h
+//  Turtle Run
+//
+//  Created by Kyle on 2/19/14.
+//  Copyright (c) 2014 USC. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Obstacle : CCSprite
+
+
+
+@end
---
+++ Turtle Run/Turtle Run/Obstacle.m
@@ -0,0 +1,13 @@
+//
+//  Obstacle.m
+//  Turtle Run
+//
+//  Created by Kyle on 2/19/14.
+//  Copyright (c) 2014 USC. All rights reserved.
+//
+
+#import "Obstacle.h"
+
+@implementation Obstacle 
+
+@end
