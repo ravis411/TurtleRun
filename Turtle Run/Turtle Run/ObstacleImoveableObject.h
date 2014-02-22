6aafc3d2d868c687eb2229ecd63b9c8fd63fadb6
ryan <ravis411@aol.com>
2014-02-21 22:16:54 -0800

    Added an ImoveableObject classneeds work.

---
+++ Turtle Run/Turtle Run/ObstacleImoveableObject.h
@@ -0,0 +1,27 @@
+//
+//  Obstacle.h
+//  Turtle Run
+//
+
+
+#import "CCSprite.h"
+#import "cocos2d.h"
+
+@interface ObstacleImoveableObject : CCSprite {
+    CCTexture2D *texSplode;
+}
+
+@property int hp;
+
++ (int) speed;
+-(void) hit:(int)damage;
+-(bool) dies;
+-(void) splode;
+-(id) init;
+-(void) travel:(ccTime)dt;
+-(void) update:(ccTime)dt;
+-(void) dealloc;
+-(CGRect) rect;
+-(BOOL) contains:(CGPoint)pt;//returns true if the point is within the obstacle
+
+@end
