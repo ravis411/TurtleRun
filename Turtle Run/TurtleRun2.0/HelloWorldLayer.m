d123cfc3c6ff7d917ff0f47601f9b56e1a7d36ac
Kyle Swinney <kmswinney@gmail.com>
2014-03-09 22:26:55 -0700

    Spritesheet Implementation
    +Clawg now called from sprite sheet

--- Turtle Run/TurtleRun2.0/HelloWorldLayer.m
+++ Turtle Run/TurtleRun2.0/HelloWorldLayer.m
@@ -72,18 +72,17 @@
         
         //BATCHING ADDITIONAL SPRITES
         CCSpriteBatchNode *firstAtlasNode;
-        firstAtlasNode = [CCSpriteBatchNode batchNodeWithFile:@"turtlesheet.png"];
+        firstAtlasNode = [CCSpriteBatchNode batchNodeWithFile:@"atlas0.png"];
         [self addChild:firstAtlasNode];
-        [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"turtlesheet.plist"];
+        [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"atlas0.plist"];
         
         NSMutableArray *firstAtlasFrames = [NSMutableArray array];
         NSArray *firstAtlasFilenames = [NSArray arrayWithObjects:@"blast.png", @"cannon_0.png", @"cannon_1.png", @"cannon_2.png", @"cannon_3.png", @"cannon_4.png", @"clawg.png", @"fpsup.png", @"oneup.png", @"scatterup.png", nil];
         //GET THIS PART TO WORK!!!!!!!!!!!!!!!!!!!!!!
-//        for (int i = 0; i <= 9; i++) {
-//            NSString *file = firstAtlasFilenames[i];
-//            CCSpriteFrame *frame = [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:file];
-//            [firstAtlasFrames addObject:frame];
-//        }
+        for (int i = 0; i <= 9; i++) {
+            CCSpriteFrame *frame = [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:firstAtlasFilenames[i]];
+            [firstAtlasFrames addObject:frame];
+        }
 
         
         
